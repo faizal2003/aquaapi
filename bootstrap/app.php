0c16b18d4e0b260e8358e8bf9faf19fbe8e95d81
faizalSan <49065101+faizal2003@users.noreply.github.com>
2024-07-15 20:09:30 +0700

    bugwlph

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,18 +17,10 @@ return Application::configure(basePath: dirname(__DIR__))
         'https://puh.web.id/feed',
         'https://puh.web.id/sensor/ph',
         'https://puh.web.id/sensor/wl',
-        'https://puh.web.id/man/ph',
-        'https://puh.web.id/man/tds',
-        'https://puh.web.id/man/turbid',
-        'https://puh.web.id/man/temp',
-        'http://aquaapi.test/sensor',
-        'http://aquaapi.test/feed',
-        'http://aquaapi.test/sensor/ph',
-        'http://aquaapi.test/sensor/wl',
-        'http://aquaapi.test/man/ph',
-        'http://aquaapi.test/man/tds',
-        'http://aquaapi.test/man/turbid',
-        'http://aquaapi.test/man/temp',
+        'puh.web.id/sensor',
+        'puh.web.id/feed',
+        'puh.web.id/sensor/ph',
+        'puh.web.id/sensor/wl',
     ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
