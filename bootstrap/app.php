d88c53707e18bc5fbe73ca37f23308d70f28e9ef
faizalSan <49065101+faizal2003@users.noreply.github.com>
2024-07-16 18:23:06 +0700

    tryfix

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,10 +17,16 @@ return Application::configure(basePath: dirname(__DIR__))
         'https://puh.web.id/feed',
         'https://puh.web.id/sensor/ph',
         'https://puh.web.id/sensor/wl',
+        'https://puh.web.id/sensor/wl',
         'puh.web.id/sensor',
         'puh.web.id/feed',
         'puh.web.id/sensor/ph',
         'puh.web.id/sensor/wl',
+        'puh.web.id/sensor/wl',
+        'http://aquaapi.test/sensor',
+        'http://aquaapi.test/feed',
+        'http://aquaapi.test/sensor/ph',
+        'http://aquaapi.test/sensor/wl',
     ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
